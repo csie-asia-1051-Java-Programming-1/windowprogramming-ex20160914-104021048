a72785e397116c93046c7dd86862b2dea37cee9e
104021048 <104021048@live.asia.edu.tw>
2016-09-18 20:45:48 +0800

    作業繳交

---
+++ src/hw01_1_104021048.java
@@ -0,0 +1,10 @@
+
+public class hw01_1_104021048 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hw01_2_104021048 hw1 = new hw01_2_104021048();
+		hw1.setVisible(true);
+	}
+
+}
